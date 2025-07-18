224028a7cf7e98ab02255f2f613739acd910d1c9
fufesou <linlong1266@gmail.com>
2025-07-18 17:28:28 +0800

    test
    Signed-off-by: fufesou <linlong1266@gmail.com>

--- lib/src/ui/custom_text_edit.dart
+++ lib/src/ui/custom_text_edit.dart
@@ -240,7 +240,7 @@ class CustomTextEditState extends State<CustomTextEdit> with TextInputClient {
 
   @override
   void performAction(TextInputAction action) {
-    // print('performAction $action');
+    print(' ============================= performAction $action');
     widget.onAction(action);
   }
 
